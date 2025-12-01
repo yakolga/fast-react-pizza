d7ce0dc3675d1efbeba6541ca63645b5ec94c965
Olga Kulczycka <olga@webjaksklep.eu>
2025-12-01 21:57:25 +0100

    feat: project structure

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,21 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+function Menu() {
+  const menu = useLoaderData();
+  return (
+    <ul>
+      {menu.map((item) => (
+        <MenuItem pizza={item} key={item.id}/>
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
