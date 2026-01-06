8e2dc59b18643b7e636b688898cb91c096733d3d
Olga Kulczycka <olga@webjaksklep.eu>
2026-01-06 22:17:01 +0100

    cart state

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './features/user/userSlice';
+import cartResucer from './features/cart/cartSlice';
 
 const store = configureStore({
     reducer: {
         user: userReducer,
+        cart: cartResucer,
     }
 })
 
